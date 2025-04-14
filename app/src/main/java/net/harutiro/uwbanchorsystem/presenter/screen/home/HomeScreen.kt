c74ef77a87b705ef5eecc062abcaa92300f4171d
harutiro <harutiro2027@outlook.jp>
2025-04-15 03:45:24 +0900

    ref: いらないimportを削除

--- app/src/main/java/net/harutiro/uwbanchorsystem/presenter/screen/home/HomeScreen.kt
+++ app/src/main/java/net/harutiro/uwbanchorsystem/presenter/screen/home/HomeScreen.kt
@@ -1,42 +1,21 @@
 package net.harutiro.uwbanchorsystem.presenter.screen.home
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccessAlarms
 import androidx.compose.material.icons.filled.Description
-import androidx.compose.material.icons.sharp.Search
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import net.harutiro.uwbanchorsystem.presenter.components.CustomOutlinedTextField
-import net.harutiro.uwbanchorsystem.presenter.components.CustomSearchBar
 import net.harutiro.uwbanchorsystem.presenter.components.CustomTopAppBar
 
 @Composable
