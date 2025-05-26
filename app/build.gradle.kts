4c3c915b2bd4272254691b50cb9244fc6ecca0ec
harutiro <harutiro2027@outlook.jp>
2025-05-27 01:44:21 +0900

    add: firebaseの導入を行なった。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -76,4 +77,8 @@ dependencies {
     implementation (libs.retrofit)
     implementation (libs.logging.interceptor)
     implementation (libs.converter.gson)
+
+    // firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
 }
